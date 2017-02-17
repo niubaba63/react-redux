9b51d12eb61e3319cbffc49d031c586d53ae29e0
niubaba63 <yczhouph@163.com>
2017-02-17 17:54:40 +0800

    加入中文注释

--- src/utils/Subscription.js
+++ src/utils/Subscription.js
@@ -4,84 +4,88 @@
 /**
  * 封装订阅逻辑 用于组件连接 redux store ，以及嵌套订阅的后代组件，以便我们可以确保
  * 祖先组件在后代之前重绘 re-render
- *
+ * 这里是用一个典型的订阅发布模式，通过对store或父级subscription进行监听，来进行组件的更新(onStateChange)。
+ * https://segmentfault.com/a/1190000007956140
  */
 const CLEARED = null
-const nullListeners = { notify() {} }
+const nullListeners = {
+    notify() {
+    }
+}
 
 function createListenerCollection() {
-  // the current/next pattern is copied from redux's createStore code.
-  // TODO: refactor+expose that code to be reusable here?
-  let current = []
-  let next = []
+    // the current/next pattern is copied from redux's createStore code.
+    // TODO: refactor+expose that code to be reusable here?
+    let current = []
+    let next = []
 
-  return {
-    clear() {
-      next = CLEARED
-      current = CLEARED
-    },
+    return {
+        clear() {
+            next = CLEARED
+            current = CLEARED
+        },
 
-    notify() {
-      const listeners = current = next
-      for (let i = 0; i < listeners.length; i++) {
-        listeners[i]()
-      }
-    },
+        notify() {
+            const listeners = current = next
+            for (let i = 0; i < listeners.length; i++) {
+                listeners[i]()
+            }
+        },
 
-    subscribe(listener) {
-      let isSubscribed = true
-      if (next === current) next = current.slice()
-      next.push(listener)
+        subscribe(listener) {
+            let isSubscribed = true
+            if (next === current) next = current.slice()
+            next.push(listener)
 
-      return function unsubscribe() {
-        if (!isSubscribed || current === CLEARED) return
-        isSubscribed = false
+            return function unsubscribe() {
+                if (!isSubscribed || current === CLEARED) return
+                isSubscribed = false
 
-        if (next === current) next = current.slice()
-        next.splice(next.indexOf(listener), 1)
-      }
+                if (next === current) next = current.slice()
+                next.splice(next.indexOf(listener), 1)
+            }
+        }
     }
-  }
 }
 
 export default class Subscription {
-  constructor(store, parentSub, onStateChange) {
-    this.store = store
-    this.parentSub = parentSub
-    this.onStateChange = onStateChange
-    this.unsubscribe = null
-    this.listeners = nullListeners
-  }
-
-  addNestedSub(listener) {
-    this.trySubscribe()
-    return this.listeners.subscribe(listener)
-  }
-
-  notifyNestedSubs() {
-    this.listeners.notify()
-  }
-
-  isSubscribed() {
-    return Boolean(this.unsubscribe)
-  }
-
-  trySubscribe() {
-    if (!this.unsubscribe) {
-      this.unsubscribe = this.parentSub
-        ? this.parentSub.addNestedSub(this.onStateChange)
-        : this.store.subscribe(this.onStateChange)
- 
-      this.listeners = createListenerCollection()
+    constructor(store, parentSub, onStateChange) {
+        this.store = store
+        this.parentSub = parentSub
+        this.onStateChange = onStateChange
+        this.unsubscribe = null
+        this.listeners = nullListeners
     }
-  }
 
-  tryUnsubscribe() {
-    if (this.unsubscribe) {
-      this.unsubscribe()
-      this.unsubscribe = null
-      this.listeners.clear()
-      this.listeners = nullListeners
+    addNestedSub(listener) {
+        this.trySubscribe()
+        return this.listeners.subscribe(listener)
     }
-  }
+
+    notifyNestedSubs() {
+        this.listeners.notify()
+    }
+
+    isSubscribed() {
+        return Boolean(this.unsubscribe)
+    }
+
+    trySubscribe() {
+        if (!this.unsubscribe) {
+            this.unsubscribe = this.parentSub
+                ? this.parentSub.addNestedSub(this.onStateChange)
+                : this.store.subscribe(this.onStateChange)
+
+            this.listeners = createListenerCollection()
+        }
+    }
+
+    tryUnsubscribe() {
+        if (this.unsubscribe) {
+            this.unsubscribe()
+            this.unsubscribe = null
+            this.listeners.clear()
+            this.listeners = nullListeners
+        }
+    }
 }
--- src/utils/verifyPlainObject.js
+++ src/utils/verifyPlainObject.js
@@ -1,10 +1,11 @@
 import isPlainObject from 'lodash/isPlainObject'
 import warning from './warning'
 
+//通过判断是否是plainObject，使用lodash来判断，并给个warning。这个方法主要是用在非production环境下，对数据格式进行检测，并抛出异常
 export default function verifyPlainObject(value, displayName, methodName) {
-  if (!isPlainObject(value)) {
-    warning(
-      `${methodName}() in ${displayName} must return a plain object. Instead received ${value}.`
-    )
-  }
+    if (!isPlainObject(value)) {
+        warning(
+            `${methodName}() in ${displayName} must return a plain object. Instead received ${value}.`
+        )
+    }
 }
